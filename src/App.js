d1420b1080004e8ad54d4492a531fc67cc44d1ee
Ramon Hernandez <ramongh331@gmail.com>
2023-02-04 00:20:41 -0500

    added spinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from "./components/Header"
+import { useState, useEffect} from 'react';
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true);
+
+  function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise(resolve => setTimeout(() => resolve(), 10000));
+  } 
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="App">
       <Header/>
